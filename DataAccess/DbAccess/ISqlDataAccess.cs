86b3aec31823e1a157e3513ed5b8a50007789b48
Lance <lance.obuyes@gmail.com>
2022-10-02 20:44:13 -0600

    Add project files.

---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccess.DbAccess;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U paramaters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T paramaters, string connectionId = "Default");
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,9 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName,LastName)
+	values ('Lance', 'Obuyes'),
+		   ('John','Smith'),
+		   ('Sue','Bird'),
+		   ('Mama','Mary');
+end
+
